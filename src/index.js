c35d4ca11c30823ce51b18dd4a3c43176f06f781
Manuel Martín-Vivaldi <manuel.martin-vivaldi@intelygenz.com>
2017-03-06 19:49:47 +0100

    heartbeat.feature done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const server = require('./server'),
+    heartbeat = require('./heartbeat.js'),
     createCheckout = require('./checkout/rest/createCheckout.js'),
     retrieveCheckout = require('./checkout/rest/retrieveCheckout.js');
 
@@ -8,6 +9,7 @@ let serverInstance = server.create();
 
 server.start(serverInstance, 3000)
     .tap(() => {
+        serverInstance.get('/api/heartbeat', heartbeat);
         serverInstance.post('/api/checkouts', createCheckout);
         serverInstance.get('/api/checkouts/:checkoutId', retrieveCheckout);
 
